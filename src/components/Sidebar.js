2b58828ad38feb56980c3ecb0002d3d6be73568a
1CarlosRenan <carlosrenan96@gmail.com>
2022-07-29 19:49:54 -0300

    Incrementando o NavBar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -7,7 +7,13 @@ import { links } from '../data/dummy';
 import { useStateContext } from '../contexts/ContextProvider';
 
 const Sidebar = () => {
-  const { activeMenu, setActiveMenu } = useStateContext();
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext();
+
+  const handleCloseSidebar = () => {
+    if (activeMenu && screenSize <= 900) {
+      setActiveMenu(false)
+    }
+  }
   const activeLink =
     'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
   const normalLink =
@@ -20,7 +26,7 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => setActiveMenu(false)}
+              onClick={handleCloseSidebar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dar:text-white text-stale-900"
             >
               <SiShopware className="text-3xl" /> <span>Shoppy</span>
@@ -45,7 +51,7 @@ const Sidebar = () => {
                   <NavLink
                     to={`/${link.name}`}
                     key={link.name}
-                    onClick={() => { }}
+                    onClick={handleCloseSidebar}
                     className={({ isActive }) =>
                       isActive ? activeLink : normalLink
                     }
