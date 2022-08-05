54d9a6de4df59986b6ced50a40d0278390ebbb48
1CarlosRenan <carlosrenan96@gmail.com>
2022-08-04 22:21:53 -0300

    Finalizando a page Ecommerce

--- src/components/Charts/SparkLine.js
+++ src/components/Charts/SparkLine.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { SparklineComponent, Inject, SparklineTooltip } from '@syncfusion/ej2-react-charts';
 
 const SparkLine = ({ id, height, width, color, data, type, currentColor }) => {
-  console.log(data)
   return (
     <SparklineComponent
       id={id}
