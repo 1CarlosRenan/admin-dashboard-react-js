6f02093d6613eeb56a82f1dcc7af09fa84451252
1CarlosRenan <carlosrenan96@gmail.com>
2022-09-28 22:44:44 -0300

    Ajustes e incrementando charts

--- src/pages/Charts/Line.js
+++ src/pages/Charts/Line.js
@@ -1,15 +1,14 @@
 import React from 'react';
-import { Header, LineChart } from '../../components';
 
-const Line = () => {
-  return (
-    <div className='m-4 md:m-10 mt-24 p-10 bg-white, dark:bg-secondary-dark-bg rounded-3xl'>
-      <Header category="Charts" title="Inflation Rate" />
-      <div className="w-full">
-        <LineChart />
-      </div>
+import { ChartsHeader, LineChart } from '../../components';
+
+const Line = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <ChartsHeader category="Line" title="Inflation Rate" />
+    <div className="w-full">
+      <LineChart />
     </div>
-  )
-}
+  </div>
+);
 
-export default Line
+export default Line;
--- src/pages/Orders.js
+++ src/pages/Orders.js
@@ -5,24 +5,27 @@ import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
 import { Header } from '../components';
 
 const Orders = () => {
+  const editing = { allowDeleting: true, allowEditing: true };
   return (
-    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
       <Header category="Page" title="Orders" />
       <GridComponent
-        id='gridcomp'
+        id="gridcomp"
         dataSource={ordersData}
         allowPaging
         allowSorting
+        allowExcelExport
+        allowPdfExport
+        contextMenuItems={contextMenuItems}
+        editSettings={editing}
       >
         <ColumnsDirective>
-          {ordersGrid.map((item, index) => (
-            <ColumnDirective key={index} {...item} />
-          ))}
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {ordersGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
         </ColumnsDirective>
         <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
       </GridComponent>
     </div>
-  )
-}
-
-export default Orders
+  );
+};
+export default Orders;
