7275dc70f5dd50a562607e198d18099cd4dd035a
1CarlosRenan <carlosrenan96@gmail.com>
2022-09-28 22:51:11 -0300

    Ajustes e incrementando novos elementos

--- src/components/Charts/LineChart.js
+++ src/components/Charts/LineChart.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, LineSeries, DateTime, Legend, Tooltip } from '@syncfusion/ej2-react-charts';
 
-import { lineCustomSeries, LinePrimaryYAxis, LinePrimaryXAxis } from '../../data/dummy';
+import { lineCustomSeries, LinePrimaryXAxis, LinePrimaryYAxis } from '../../data/dummy';
 import { useStateContext } from '../../contexts/ContextProvider';
 
 const LineChart = () => {
   const { currentMode } = useStateContext();
+
   return (
     <ChartComponent
       id="line-chart"
@@ -15,15 +16,15 @@ const LineChart = () => {
       chartArea={{ border: { width: 0 } }}
       tooltip={{ enable: true }}
       background={currentMode === 'Dark' ? '#33373E' : '#fff'}
+      legendSettings={{ background: 'white' }}
     >
       <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
       <SeriesCollectionDirective>
-        {lineCustomSeries.map((item, index) =>
-          <SeriesDirective key={index} {...item} />
-        )}
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {lineCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
       </SeriesCollectionDirective>
     </ChartComponent>
-  )
-}
+  );
+};
 
-export default LineChart
+export default LineChart;
